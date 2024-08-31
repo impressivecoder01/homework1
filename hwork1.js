9ca1d05c9029634d66cec4a32cbe3087104d401d
impressivecoder01 <rohanrahman251@gmail.com>
2024-08-31 22:47:30 +0600

    first commit

---
+++ hwork1.js
@@ -0,0 +1,34 @@
+// task 1       Free Drinks
+
+// let burger = 333;
+// if(burger > 500){
+//     console.log("free coke")
+// }
+// else{
+//     console.log("coke 30tk")
+// }
+
+// task 2
+
+
+// task 3      Grade Calculator
+// const grade = (0);
+
+// if(grade>=90 && grade<=100){
+//     console.log("Got A+")
+// }
+// if(grade>=80 && grade<=89){
+//     console.log("Got A")
+// }
+// if(grade>=70 && grade<=79){
+//     console.log("Got B+")
+// }
+// if(grade>=60 && grade<=69){
+//     console.log("B")
+// }
+// else{
+//     console.log("c")
+// }
+// console.log(grade);
+
+
