72f0a6c0289586899b9ef8fed909b95c940a1a92
impressivecoder01 <rohanrahman251@gmail.com>
2024-09-01 03:20:21 +0600

    issue

---
+++ hwork2.js
@@ -0,0 +1,23 @@
+// ### 1. Declare an array 
+// 1. Declare an array with 5 elements containing fruits
+// 2. console log the 3rd index element
+// 3. change the value of the 2nd index element to jambura
+// 4. console log the final array
+// let fruits = ["mango , apple, banana, orange, biskut"];
+// let three = fruits[3]
+// console.log(three)
+
+// 1. Declare an array of 3 tourist destinations
+// 2. Add a new tourist destination to your tourist array
+// 3. Add two more to your array
+// 4. Remove the last tourist destination you have added
+// 5. display the final array as output
+
+// task 2
+let spot = ["bd , usa , uk"]
+spot.push("canada")
+console.log(spot)
+spot.push("a, b")
+console.log(spot)
+spot.pop()
+console.log(spot)
