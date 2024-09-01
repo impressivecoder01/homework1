f419f027fc132d3e851623c4ce319126ef54d08b
impressivecoder01 <rohanrahman251@gmail.com>
2024-09-01 22:33:23 +0600

    task 2 done

--- hwork2.js
+++ hwork2.js
@@ -1,23 +1,44 @@
 // ### 1. Declare an array 
-// 1. Declare an array with 5 elements containing fruits
-// 2. console log the 3rd index element
-// 3. change the value of the 2nd index element to jambura
-// 4. console log the final array
-// let fruits = ["mango , apple, banana, orange, biskut"];
-// let three = fruits[3]
-// console.log(three)
+let fruits = ["mango" , "apple", "banana", "orange", "biskut"];
+let three = fruits[3]
+// get element value by index
+console.log(three)
+// set or update index value by index
+fruits[2] = "jambura"
+console.log(fruits);
 
-// 1. Declare an array of 3 tourist destinations
-// 2. Add a new tourist destination to your tourist array
-// 3. Add two more to your array
-// 4. Remove the last tourist destination you have added
-// 5. display the final array as output
 
 // task 2
-let spot = ["bd , usa , uk"]
+let spot = ["bd" , "usa" , "uk"]
 spot.push("canada")
 console.log(spot)
-spot.push("a, b")
+spot.push("a", "b")
 console.log(spot)
 spot.pop()
 console.log(spot)
+
+// task 3
+let book = ['a', 'b', 'c', 'd', 'e']
+if(book.includes('i')){
+    console.log("got it")
+}
+else{
+    console.log("no idea")
+}
+
+// task 4
+let ar = [1 , 3, 5 , 6 , 67, 64];
+let ur = 34;
+if(Array.isArray(ar)){
+    console.log("yes it is")
+}
+else{
+    console.log("no")
+}
+
+// task 5
+const first = [1, 2, 3, 4, 5, 6, 7, 8];
+const second = [9, 10, 11, 12, 13, 14, 15, 16 ] ;
+const third = [4]
+const forth = first.concat(second , third);
+console.log(forth);
