1c435f05263572fc4c49e0d5e42a0e12d231bc58
impressivecoder01 <rohanrahman251@gmail.com>
2024-09-04 02:33:33 +0600

    h1

--- hwork1.js
+++ hwork1.js
@@ -30,25 +30,25 @@
 // else{
 //     console.log("c")
 // }
-let grade = 75;
-if (grade >= 90 && grade <=100){
-    console.log("Got a+")
-}
-else if(grade >= 80 && grade<= 89){
-    console.log("a")
-}
-else if(grade >= 70 && grade <= 79){
-    console.log("b+")
-}
-else if(grade >= 60 && grade <= 69){
-    console.log("b")
-}
-else if(grade >= 0 && grade <= 59){
-    console.log("c")
-}
-else{
-    console.log("sleep")
-}
+// let grade = 75;
+// if (grade >= 90 && grade <=100){
+//     console.log("Got a+")
+// }
+// else if(grade >= 80 && grade<= 89){
+//     console.log("a")
+// }
+// else if(grade >= 70 && grade <= 79){
+//     console.log("b+")
+// }
+// else if(grade >= 60 && grade <= 69){
+//     console.log("b")
+// }
+// else if(grade >= 0 && grade <= 59){
+//     console.log("c")
+// }
+// else{
+//     console.log("sleep")
+// }
 
 
 // task 4      nested friend
@@ -72,34 +72,51 @@ else{
 
 // console.log(markFriend)
 
-
+// let markFriend = 45;
+// let markMe = 5;
+// if(markFriend > 80 && markMe > 80){
+//     console.log("let have a lunch.")
+// }
+// else if((markFriend <= 80 && markFriend >= 60)&& (markMe > 80)){
+//     console.log("better luck");
+// }
+// else if((markFriend < 60 && markFriend >= 40)&& (markMe > 80)){
+//     console.log("no msg")
+// }
+// else if((markFriend < 40 && markMe > 80) && (markMe > 80)){
+//     console.log("blk")
+// }
+// else if(markMe < 80){
+//     console.log("go to sleep")
+// }
 // task 5
-// let one = 5;
+// let one =7;
 // let two = 6;
-// let result = one + two;
+// let result;
 // if(one > two){
-//     console.log(one * 2)
+//     result = one * 2;
 // }
 // else{
-//     console.log(result)
+//     result = one + two;
 // }
+// console.log(result);
 
 // ternary
 // one > two ? console.log(one *2 ) : console.log(result)
 
 // task 6   ticket fare calculator
-// let age = 4;
-// let ticket = 200;
-// let student = true;
-// if(age < 10){
-//     console.log("free")
-// }
-// else if(student && age > 11){
-//     console.log(ticket * 50 / 100)
-// }
-// else if (age >= 60 && age < 80){
-//     console.log(ticket * 15 / 100)
-// }
-// else{
-//     console.log(ticket)
-// }
+// let age = 22;
+let ticket = 200;
+let student = true;
+if(age < 10){
+    console.log("free")
+}
+else if(student ){
+    console.log(ticket * 50 / 100)
+}
+else if (age >= 60 && age < 80){
+    console.log(ticket * 15 / 100)
+}
+else{
+    console.log(ticket)
+}
