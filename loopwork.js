6a76651cabd75ad6dc8b2586d412bd485e2ed991
impressivecoder01 <rohanrahman251@gmail.com>
2024-09-05 22:17:55 +0600

    while loop task 1-3 done

--- loopwork.js
+++ loopwork.js
@@ -1,8 +1,53 @@
-// task 1
-let message = 0;
-while(message <= 60){
-    console.log("I will invest at least 6 hrs every single day for next 60 days!");
-    message++;
-}
+// task 1 
 
+// while loop
 
+// let message = 0;
+// while(message <= 60){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+//     message++;
+// }
+
+// task 2 
+// odd number from 61 to 100
+
+// let oddNum = 61;
+// while(oddNum <= 100){
+//     if(oddNum % 2 !== 0){
+//         console.log(oddNum);
+//     }
+//     oddNum++;
+// }
+// even number from 78 to 98
+
+// let evenNumber = 78;
+// while(evenNumber <= 98){
+//     if(evenNumber%2 === 0){
+//         console.log(evenNumber);
+//     }
+//     evenNumber++;
+// }
+
+// display sum of all odd numbers from 81 to 131
+
+// let oddSumNum = 81;
+// let oddSum = 0;
+// while(oddSumNum <= 131){
+//     if(oddSumNum%2 !== 0){
+//         oddSum = oddSum + oddSumNum;
+//         // console.log(oddSumNum);
+//     }
+//     oddSumNum++;
+// }
+// console.log(oddSum);
+
+// display sum of all even numbers from 206 to 311
+// let evenSumNmb = 206;
+// let evenSum = 0;
+// while(evenSumNmb <= 311){
+//     if(evenSumNmb%2 === 0){
+//         evenSum = evenSum + evenSumNmb;
+//     }
+//     evenSumNmb++;
+// }
+// console.log(evenSum);
