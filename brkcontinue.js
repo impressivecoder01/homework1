8e2d1097b9ece32520215fa6a135f7de4923db4e
impressivecoder01 <rohanrahman251@gmail.com>
2024-09-07 02:03:05 +0600

    task done

--- brkcontinue.js
+++ brkcontinue.js
@@ -23,12 +23,71 @@
 //     }
 //     console.log(i);
 // }
+// issue
+// let a = 0;
+// while(a <= 5){
+//     a++;
+//     if(a%3 === 0 && a%5 === 0){
+//         continue;
+//     }
+//     console.log(a);
+// }
+// issue
 
-let a = 0;
-while(a <= 5){
-    a++;
-    if(a%3 === 0 && a%5 === 0){
-        continue;
-    }
-    console.log(a);
-}
+// task 1 break
+// let a = 1; 
+// while(a < 200){
+//     if(a >= 100){
+//         break;
+//     }
+//     a++;
+//     console.log(a)
+// }
+
+
+// task 2 break
+
+// let i = 1;
+// let sum = 0;
+// while(i <= 500){
+//     i++;
+//     sum = sum + i;
+//     if(sum >= 100){
+//         break;
+//     }
+//     console.log(i);
+// }
+// console.log(sum);
+
+// why output is 104
+
+// task 3 break
+
+// for(i=1; i <=100; i++){
+//     if(i*2 === 4){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// continue
+
+// task 1
+// for(i = 1; i <=40; i++){
+//     if(i%2 !== 0){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// task 2
+
+// for(i = 55; i <=85; i++){
+//     if(i %2 !== 0){
+//         if(i % 5 === 0){
+//             continue;
+//         }
+//     }
+//     console.log(i);
+// }
